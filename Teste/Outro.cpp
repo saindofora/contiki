63b2b07f2726a571194f41e6d9ef633fbdb28199
james <saindofora@hotmail.com>
2018-03-09 22:31:44 -0300

    t

---
+++ Teste/Outro.cpp
@@ -0,0 +1,20 @@
+/*
+ * Outro.cpp
+ *
+ *  Created on: Mar 9, 2018
+ *      Author: mint
+ */
+
+#include "Outro.h"
+
+Outro::Outro()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Outro::~Outro()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ Teste/Outro.h
@@ -0,0 +1,18 @@
+/*
+ * Outro.h
+ *
+ *  Created on: Mar 9, 2018
+ *      Author: mint
+ */
+
+#ifndef TESTE_OUTRO_H_
+#define TESTE_OUTRO_H_
+
+class Outro
+{
+public:
+    Outro();
+    virtual ~Outro();
+};
+
+#endif /* TESTE_OUTRO_H_ */
