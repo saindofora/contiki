b709ba5af19f6045312160cc7da609f059609b24
james <saindofora@hotmail.com>
2018-03-09 22:20:40 -0300

    re

---
+++ examples/hello-world/Teste.cpp
@@ -0,0 +1,20 @@
+/*
+ * Teste.cpp
+ *
+ *  Created on: Mar 9, 2018
+ *      Author: mint
+ */
+
+#include "Teste.h"
+
+Teste::Teste()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Teste::~Teste()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ examples/hello-world/Teste.h
@@ -0,0 +1,18 @@
+/*
+ * Teste.h
+ *
+ *  Created on: Mar 9, 2018
+ *      Author: mint
+ */
+
+#ifndef TESTE_H_
+#define TESTE_H_
+
+class Teste
+{
+public:
+    Teste();
+    virtual ~Teste();
+};
+
+#endif /* TESTE_H_ */
